9eb709f4b5267aa9fa8a99670246432bd9fcd84d
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2018-02-10 19:18:16 -0500

    Added Router and Homepage

---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <h2>404</h2>
+        <p>Sorry, the resource you were looking for does not exist.</p>
+      </div>
+    )
+  }
+}
+
+export default NotFound
